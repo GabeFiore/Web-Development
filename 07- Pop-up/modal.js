db8df2b8c6eb3f82e122e29cc4925b363a370734
Gabriel Fiore <gabriel_fiore@hotmail.com>
2024-05-04 16:32:42 -0300

    modal

--- 07- Pop-up/modal.js
+++ 07- Pop-up/modal.js
@@ -1,17 +1,19 @@
 const button = document.querySelector("button");
-const poupup = document.querySelector(".poupup-wrapper");
+const popup = document.querySelector(".popup-wrapper");
 
-const openClick = function(){poupup.classList.add('d-block');}
-const eventModal = function(event) {
-    const ClickOfElement = event.target.classList[0];
-    const clasNameListArray = ["popup-close", "poupup-link", "poupup-wrapper"];
+const openClick = function () {
+  popup.classList.add("d-block");
+};
+const eventModal = function (event) {
+  const ClickOfElement = event.target.classList[0];
+  const clasNameListArray = ["popup-close", "popup-link", "popup-wrapper"];
 
-    const isClassList = clasNameListArray.includes(ClickOfElement);
-  
-    if(isClassList) {
-      poupup.classList.remove('d-block');
-    }
-}
+  const isClassList = clasNameListArray.includes(ClickOfElement);
+
+  if (isClassList) {
+    popup.classList.remove("d-block");
+  }
+};
 
 button.addEventListener("click", openClick);
-poupup.addEventListener("click", eventModal);
+popup.addEventListener("click", eventModal);
